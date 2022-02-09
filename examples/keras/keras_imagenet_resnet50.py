3c870e7a72feb37b6881bf5563df17855a7a6e0c
George Saunderson <91902558+gsaunderson-acn@users.noreply.github.com>
2022-02-09 15:55:32 -0800

    Update keras_imagenet_resnet50.py

--- examples/keras/keras_imagenet_resnet50.py
+++ examples/keras/keras_imagenet_resnet50.py
@@ -30,9 +30,9 @@ import os
 
 parser = argparse.ArgumentParser(description='Keras ImageNet Example',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--train-dir', default=os.path.expanduser('~/imagenet/train'),
+parser.add_argument('--train_dir', default=os.path.expanduser('/home/ubuntu/imagenet/train'),
                     help='path to training data')
-parser.add_argument('--val-dir', default=os.path.expanduser('~/imagenet/validation'),
+parser.add_argument('--val_dir', default=os.path.expanduser('/home/ubuntu/imagenet/validation'),
                     help='path to validation data')
 parser.add_argument('--log-dir', default='./logs',
                     help='tensorboard log directory')
